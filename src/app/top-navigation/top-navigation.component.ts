5263a390bf8b3db19298b9d4dc40c3d8de2a0e31
Ne-anna <annabusele@inbox.lv>
2022-09-01 15:21:04 +0300

    Create navigation component

---
+++ src/app/top-navigation/top-navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import data from '../data.json';
+
+interface NavigationData {
+  title: string;
+  menuIcon: string;
+  cartIcon: string;
+}
+
+@Component({
+  selector: 'app-top-navigation',
+  templateUrl: './top-navigation.component.html',
+  styleUrls: ['./top-navigation.component.sass'],
+})
+export class TopNavigationComponent implements OnInit {
+  public data: NavigationData = data;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
