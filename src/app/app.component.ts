4f4fed9cdec9b5542d7e4f639d9533f879af0a99
Ne-anna <annabusele@inbox.lv>
2022-09-01 15:20:51 +0300

    Change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import data from './data.json';
 
+interface Data {
+  text: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'bookstore-angular';
+  data: Data = data;
 }
