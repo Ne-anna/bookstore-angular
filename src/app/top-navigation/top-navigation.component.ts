fad626fc5f58655d9b56f435496a4d1cd61d7927
Ne-anna <annabusele@inbox.lv>
2022-09-06 11:57:50 +0300

    Import inteface, change path

--- src/app/top-navigation/top-navigation.component.ts
+++ src/app/top-navigation/top-navigation.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import data from '../data.json';
-
-interface NavigationData {
-  title: string;
-  menuIcon: string;
-  cartIcon: string;
-}
-
+import { AllData } from '../data';
 @Component({
   selector: 'app-top-navigation',
   templateUrl: './top-navigation.component.html',
   styleUrls: ['./top-navigation.component.scss'],
 })
 export class TopNavigationComponent {
-  public data: NavigationData = data;
+  public data: AllData = data;
 }
