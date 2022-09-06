ef32811d4aa8a96e2c55304c27a183214e23112f
Ne-anna <annabusele@inbox.lv>
2022-09-06 12:00:13 +0300

    Import bookDetails and bookList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
 @NgModule({
-  declarations: [AppComponent, TopNavigationComponent],
+  declarations: [AppComponent, TopNavigationComponent, BookListComponent, BookDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
