52c32b827f6802cec7660194dea3c45ea9fadb43
Ne-anna <annabusele@inbox.lv>
2022-09-09 12:03:13 +0300

    Create book-detail component

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import ImageDataJson from '../data.json';
-import { AllData } from '../data';
+import { Component } from '@angular/core';
+import { BookData } from '../data';
+import { BookService } from '../services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
@@ -8,5 +9,18 @@ import { AllData } from '../data';
   styleUrls: ['./book-details.component.scss'],
 })
 export class BookDetailsComponent {
-  public data: AllData = ImageDataJson;
+  public book: BookData | undefined;
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute
+  ) {}
+
+  getBooks(): void {
+    let bookId = parseInt(this.route.snapshot.params['id']);
+    this.bookService.getById(bookId).subscribe((book) => (this.book = book));
+  }
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
 }
